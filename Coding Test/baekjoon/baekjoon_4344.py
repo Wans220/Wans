99aa4b7141093d92fc4e70f0a440ea0b2e446294
Wans220 <wansik.kim220@gmail.com>
2023-08-09 18:03:28 +0900

    baekjoon

---
+++ Coding Test/baekjoon/baekjoon_4344.py
@@ -0,0 +1,15 @@
+# sol_1
+
+answer_ary = []
+for _ in range(int(input())):
+    score_ary = list(map(int, input().split()))
+    avg = (sum(score_ary) - score_ary[0])/score_ary[0]
+    proportion = 0
+    for i in range(score_ary[0]):
+        if score_ary[i+1] > avg:
+            proportion += 1
+    answer_ary.append(proportion/score_ary[0])    
+[print("%f%%" %(s*100)) for s in answer_ary]
+    
+    
+# sol_2
